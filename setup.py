c9e1775ee04fe2dc4bda2dc08396e05bedef5209
Jordan McCoy <mccoy.jordan@gmail.com>
2012-03-11 11:56:48 -0500

    a bit of housekeeping

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='bake',
-    version='1.0.a1',
-    description='A project scripting utility.',
+    version='1.0.0a1',
+    description='A project scripting and build utility.',
     long_description=open('README.rst').read(),
     author='Jordan McCoy',
     author_email='mccoy.jordan@gmail.com',
@@ -11,4 +11,14 @@ setup(
     url='http://github.com/jordanm/bake',
     packages=['bake', 'bake.lib'],
     scripts=['bin/bake'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Utilities',
+    ]
 )
