bccd1c862b657c75934c0ac04bc07ac51fa10b68
Aaron Colichia <colichia@us.ibm.com>
2015-04-06 15:50:12 -0500

    Improvement 6428 add allow for dir symmlinks to be included in collation.prune

--- bake/filesystem.py
+++ bake/filesystem.py
@@ -3,11 +3,15 @@ import tarfile
 from bake.path import path
 
 class Collation(object):
-    def __init__(self, root):
+    def __init__(self, root, runtime=None):
         if not isinstance(root, path):
             root = path(root)
 
         self.root = root.abspath()
+        if runtime: # added for debugging purposes
+            self.runtime = runtime
+        else:
+            self.runtime = None
         self.collate()
 
     @property
@@ -21,21 +25,36 @@ class Collation(object):
         for subject in self.root.walk():
             subject = subject.abspath()
             if subject.isdir():
+                if self.runtime:
+                    self.runtime.report('filesystem Collation.collate: %s is a dir, adding to directories' % subject)
                 self.directories.append(str(subject))
-            elif subject.isfile() or subject.islink():
+            elif subject.isfile():
+                if self.runtime:
+                    self.runtime.report('filesystem Collation.collate: %s is a file, adding to files with hash %s' % (subject, subject.read_hexhash('sha1')))
                 self.files[str(subject)] = subject.read_hexhash('sha1')
 
     def prune(self, other):
         for filepath, hash in self.files.items():
             if other.files.get(filepath) == hash:
                 del self.files[filepath]
+        
+        for directory in self.directories:
+            if self.runtime:
+                self.runtime.report('checking for %s in the other collation' % directory)
+        
+            if directory in other.directories:
+                if self.runtime:
+                    self.runtime.report('removing %s from this collations directories list' % directory)
+                del self.directories[self.directories.index(directory)]
+            else:
+                if self.runtime:
+                    self.runtime.report('not removing %s from this collations directories list' % directory)
 
         filepaths = self.files.keys()
         directories = []
-
         for directory in self.directories:
             for filepath in filepaths:
-                if filepath.startswith(directory):
+                if filepath.startswith(directory) or path(directory).islink():
                     directories.append(directory)
                     break
     
@@ -54,9 +73,22 @@ class Collation(object):
         try:
             for filepath in self.filepaths:
                 arcname = filepath
-                if transforms:
+                if transforms: 
                     arcname = self._transform_filepath(filepath, transforms)
                 openfile.add(filepath, arcname, recursive=False)
+            for directory in self.directories:
+                docontinue = False
+                for filepath in self.filepaths:
+                    if filepath.startswith(directory):
+                        docontinue = True
+                        break
+                if docontinue:
+                    continue
+                arcname = directory
+                if transforms:
+                    arcname = self._transform_filepath(directory, transforms)
+                openfile.add(directory, arcname, recursive=False)
+                
         finally:
             openfile.close()
 
